01a870f0ad0b17a785d81987f0270154bd499c4d
Sairaj Jadhav <saairajj812@gmail.com>
2021-01-16 21:03:05 +0530

    Solution problem 2 in JS

---
+++ Problem#5/problem#5.js
@@ -0,0 +1,15 @@
+
+function runningSum( nums) {
+  let ans = []
+  let check = 0 
+  for(let i=0; i<nums.length; i++) {
+      check += nums[i]
+      ans.push(check)
+  }
+  return ans 
+}
+
+
+let nums = [1, 2, 3, 4]
+let ans = runningSum(nums)
+console.log(ans)
